13a2dec4563a1bda6efa8a85ebd42d670dfaedd5
nasitra <nasitra@users.noreply.github.com>
2016-01-28 18:45:43 +0900

    Make accessor of each element of canvas

--- js/components/canvas.js
+++ js/components/canvas.js
@@ -2,11 +2,22 @@
   'use strict';
 
   var helper = app.helper || require('../helper.js');
+  var dom = app.dom || require('../dom.js');
   var Component = app.Component || require('./component.js');
 
-  var Canvas = helper.inherits(function() {
+  var Canvas = helper.inherits(function(props) {
     Canvas.super_.call(this);
+
+    this.element = this.prop(props.element);
   }, Component);
+
+  Canvas.prototype.containerElement = function() {
+    return dom.child(this.element(), 0);
+  };
+
+  Canvas.prototype.handleElement = function() {
+    return dom.child(this.element(), 1);
+  };
 
   if (typeof module !== 'undefined' && module.exports)
     module.exports = Canvas;
