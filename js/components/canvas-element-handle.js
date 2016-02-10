60340f26007f2c796af3f9eee0cdc77c89681aa1
nasitra <nasitra@users.noreply.github.com>
2016-02-11 07:30:12 +0900

    Make canvas-element-handle drawing with the position and the size

--- js/components/canvas-element-handle.js
+++ js/components/canvas-element-handle.js
@@ -2,13 +2,30 @@
   'use strict';
 
   var helper = app.helper || require('../helper.js');
+  var dom = app.dom || require('../dom.js');
   var Component = app.Component || require('./component.js');
 
   var CanvasElementHandle = helper.inherits(function(props) {
     CanvasElementHandle.super_.call(this);
 
+    this.x = this.prop(0);
+    this.y = this.prop(0);
+    this.width = this.prop(0);
+    this.height = this.prop(0);
     this.element = this.prop(props.element);
   }, Component);
+
+  CanvasElementHandle.prototype.redraw = function() {
+    var translate = 'translate(' + this.x() + 'px, ' + this.y() + 'px)';
+
+    dom.css(this.element(), {
+      height: this.height() + 'px',
+      msTransform: translate,
+      transform: translate,
+      webkitTransform: translate,
+      width: this.width() + 'px'
+    });
+  };
 
   if (typeof module !== 'undefined' && module.exports)
     module.exports = CanvasElementHandle;
