20859219b8d5dc50624c91b9c0808cc5dbcba6d0
nasitra <nasitra@users.noreply.github.com>
2016-02-29 18:24:15 +0900

    Make accessor of each element of publish-form

--- js/components/publish-form.js
+++ js/components/publish-form.js
@@ -2,11 +2,22 @@
   'use strict';
 
   var helper = app.helper || require('../helper.js');
+  var dom = app.dom || require('../dom.js');
   var Component = app.Component || require('./component.js');
 
-  var PublishForm = helper.inherits(function() {
+  var PublishForm = helper.inherits(function(props) {
     PublishForm.super_.call(this);
+
+    this.element = this.prop(props.element);
   }, Component);
+
+  PublishForm.prototype.textElement = function() {
+    return dom.child(this.element(), 0);
+  };
+
+  PublishForm.prototype.buttonElement = function() {
+    return dom.child(this.element(), 1);
+  };
 
   if (typeof module !== 'undefined' && module.exports)
     module.exports = PublishForm;
