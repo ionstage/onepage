64592e20eff8779ab1243c77a6d1a5c97bc0f6e9
nasitra <nasitra@users.noreply.github.com>
2016-03-04 19:23:17 +0900

    Enable changing the fragment identifier of the URL

--- js/components/url-component.js
+++ js/components/url-component.js
@@ -2,11 +2,18 @@
   'use strict';
 
   var helper = app.helper || require('../helper.js');
+  var dom = app.dom || require('../dom.js');
   var Component = app.Component || require('./component.js');
 
   var URLComponent = helper.inherits(function() {
     URLComponent.super_.call(this);
+
+    this.fragment = this.prop(dom.urlFragment());
   }, Component);
+
+  URLComponent.prototype.redraw = function() {
+    dom.urlFragment(this.fragment())
+  };
 
   if (typeof module !== 'undefined' && module.exports)
     module.exports = URLComponent;
