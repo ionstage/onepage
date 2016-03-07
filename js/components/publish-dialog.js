659b54e0592533bd09e44edf7abaa91435329111
nasitra <nasitra@users.noreply.github.com>
2016-03-07 18:36:15 +0900

    Modify method to show and hide publish-dialog

--- js/components/publish-dialog.js
+++ js/components/publish-dialog.js
@@ -2,11 +2,22 @@
   'use strict';
 
   var helper = app.helper || require('../helper.js');
+  var dom = app.dom || require('../dom.js');
   var Component = app.Component || require('./component.js');
 
-  var PublishDialog = helper.inherits(function() {
+  var PublishDialog = helper.inherits(function(props) {
     PublishDialog.super_.call(this);
+
+    this.visible = this.prop(false);
+    this.element = this.prop(props.element);
   }, Component);
+
+  PublishDialog.prototype.redraw = function() {
+    if (this.visible())
+      dom.removeClass(this.element(), 'hide');
+    else
+      dom.addClass(this.element(), 'hide');
+  };
 
   if (typeof module !== 'undefined' && module.exports)
     module.exports = PublishDialog;
